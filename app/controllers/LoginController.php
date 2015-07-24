46de6334579155450fecf9fe9b9f4fd8f4108b49
kyle roux <kyle@level2designs.com>
2015-07-24 16:36:10 -0700

    c

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -9,6 +9,7 @@ class LoginController extends ControllerBase
         }
         $this->view->form = new LoginForm();
         $this->view->form->initalize();
+        $this->view->form_file = 'layouts/message.volt';
         echo $this->view->render('index');
     }
 }
