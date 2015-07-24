6874e291cd602cf12f44f366a923eab235b0e07e
kyle roux <kyle@level2designs.com>
2015-07-24 14:56:48 -0700

    x

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,7 +5,7 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-
+        echo (new \Phalcon\Debug\Dump())->variable($this,'this');
     }
     public function signupAction(){
         echo 'hi';
