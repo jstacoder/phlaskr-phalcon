23ef5e087a79f26e8ec6050a0a698c2c0e44a7d9
kyle roux <kyle@level2designs.com>
2015-07-23 18:48:07 -0700

    db

---
+++ app/migrations/1.0.1/messages.php
@@ -0,0 +1,86 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+class MessagesMigration_101 extends Migration
+{
+
+    public function up()
+    {
+        $this->morphTable(
+            'messages',
+            array(
+            'columns' => array(
+                new Column(
+                    'date_added',
+                    array(
+                        'type' => Column::TYPE_DATE,
+                        'notNull'=>false,
+                        'default'=> date("Y-m-d H:i:s"),
+                        'after' => 'author_id'
+                    )
+                ),
+                new Column(
+                    'id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'notNull' => true,
+                        'autoIncrement' => true,
+                        'size' => 10,
+                        'first' => true
+                    )
+                ),
+                new Column(
+                    'title',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 70,
+                        'after' => 'id'
+                    )
+                ),
+                new Column(
+                    'text',
+                    array(
+                        'type' => Column::TYPE_VARCHAR,
+                        'notNull' => true,
+                        'size' => 500,
+                        'after' => 'title'
+                    )
+                ),
+                new Column(
+                    'author_id',
+                    array(
+                        'type' => Column::TYPE_INTEGER,
+                        'unsigned' => true,
+                        'size' => 10,
+                        'after' => 'text'
+                    )
+                )
+            ),
+            'indexes' => array(
+                new Index('PRIMARY', array('id')),
+                new Index('fk_user_msg', array('author_id'))
+            ),
+            'references' => array(
+                new Reference('fk_user_msg', array(
+                    'referencedSchema' => 'testing55',
+                    'referencedTable' => 'users',
+                    'columns' => array('author_id'),
+                    'referencedColumns' => array('id')
+                ))
+            ),
+            'options' => array(
+                'TABLE_TYPE' => 'BASE TABLE',
+                'AUTO_INCREMENT' => '34',
+                'ENGINE' => 'InnoDB',
+                'TABLE_COLLATION' => 'utf8_general_ci'
+            )
+        )
+        );
+    }
+}
