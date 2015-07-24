e3fd0a9decbc3f1952170f5bf1314d73c1dc9834
kyle roux <kyle@level2designs.com>
2015-07-24 11:52:44 -0700

    adding

---
+++ app/controllers/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+class LoginController extends ControllerBase
+{
+    public function indexAction(){
+        $formClass = 'LoginForm';
+        if($this->request->isPost()){
+            $user = User::findFirstByEmail($this->request->getPost(),array('email'));
+        }
+        $this->view->form = new LoginForm();
+        $this->view->form->initalize();
+        echo $this->view->render('login/index');
+    }
+}
