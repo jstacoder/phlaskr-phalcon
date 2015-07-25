aafa6e6987909a5063cfd0c71147568d7d77f2c7
kyle roux <kyle@level2designs.com>
2015-07-25 01:16:59 -0700

    x

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -12,4 +12,17 @@ class LoginController extends ControllerBase
         $this->view->form_file = 'layouts/login.volt';
         echo $this->view->render('login/index');
     }
+    public function authAction(){
+        if(!$this->request->isPost()){
+            $this->dispatcher->forward(
+                array(
+                    'controller'=>'login',
+                    'action'=>'index'
+                )
+            );
+        }else{
+            $user = User::findFirstByEmail($this->request->getPost(),array('email'));
+            echo (new \Phalcon\Debug\Dump())->variable($user,'user');
+        }
+    }
 }
