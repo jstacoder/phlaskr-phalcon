fa4bd52237e2d8743dbaca1bd33d4fab2c2df63f
kyle roux <kyle@level2designs.com>
2015-07-25 00:53:03 -0700

    c;

--- app/forms/BaseForm.php
+++ app/forms/BaseForm.php
@@ -5,7 +5,7 @@ class BaseForm extends Form{
     public function add_field($field_class,$name,$id=null,$label=null){
         $id = is_null($id) ? $name : $id;
         $label = is_null($label) ? $name : $label;
-        $field = new $field_class($name,array('id'=>$id));
+        $field = new $field_class($name,array('id'=>$id,'class'=>'form-control'));
         $field->setLabel($label);
         $this->add($field);
     }
