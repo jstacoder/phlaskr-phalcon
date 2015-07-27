e35d4e34f3ac218f9f61389892d58ba743a0c8ce
kyle roux <kyle@level2designs.com>
2015-07-27 12:10:06 -0700

    fixs

--- app/controllers/SignupController.php
+++ app/controllers/SignupController.php
@@ -4,28 +4,22 @@ class SignupController extends ControllerBase{
     public function indexAction(){
         $this->view->form = new SignupForm();
         $this->view->form_file = 'signup/index.volt';
-
         echo $this->view->render('signup');
     }
     public function registerAction(){
-        echo '<link rel=stylesheet href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />';
-        $user = new Users();
-        
-        if($this->request->getPost('password') == $this->request->getPost('confirm')){                
-
-            $success = $user->save($this->request->getPost(),array('name','email','password'));
-
-            if($success){
-                //echo '<pre>';
-                echo $this->flash->success("Thanks for registering $user->email");
-                //echo "</pre>";
-                //echo "Thanks for registering<br/>$user->email<br/>";
+        //echo '<link rel=stylesheet href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />';
+        $user = new Users();    
+        if($this->request->getPost('password') == $this->request->getPost('confirm')){
+            $user->name = $this->request->getPost('name');
+            $user->email = $this->request->getPost('email');
+            $user->setPassword($this->request->getPost('password'));
+            $success = $user->save();
+            if($success){                
+                echo $this->flash->success("Thanks for registering $user->email");                                
             }else{  
-                echo $this->flash->error("error");            
-                //echo "error<br/>";
+                echo $this->flash->error("error");                            
                 foreach($user->getMessages() as $msg){
-                    echo $this->flash->error($msg);
-                    //echo $msg . '<br />';
+                    echo $this->flash->error($msg);                    
                 }
             }
             $this->dispatcher->forward(
