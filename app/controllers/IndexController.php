11962a33957673de4d1dc18cf433412b5c80d717
kyle roux <kyle@level2designs.com>
2015-07-27 12:23:15 -0700

    fixing login

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,9 +5,10 @@ class IndexController extends ControllerBase
 
     public function indexAction()
     {
-        echo (new \Phalcon\Debug\Dump())->variable($this,'this');
-        echo (new \Phalcon\Debug\Dump())->variable($this->request,'request');
-        echo (new \Phalcon\Debug\Dump())->variable($this->view,'view');
+        //echo (new \Phalcon\Debug\Dump())->variable($this,'this');
+        //echo (new \Phalcon\Debug\Dump())->variable($this->request,'request');
+        //echo (new \Phalcon\Debug\Dump())->variable($this->view,'view');
+        echo $this->view->render('index/index');
 
     }
     public function signupAction(){
