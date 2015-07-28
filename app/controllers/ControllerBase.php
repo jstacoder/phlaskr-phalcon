b4ce13f12063ed6442c6cfe8d3ef33cd37bc33c5
kyle roux <kyle@level2designs.com>
2015-07-27 17:04:13 -0700

    x

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -6,6 +6,7 @@ class ControllerBase extends Controller
 {
 
     private static $_links = null;
+    private static $_view = null;
     protected static $navlinks = array(
                 'home'=>
                 array(
@@ -28,28 +29,21 @@ class ControllerBase extends Controller
             );
 
     protected static function _set_active($name){
-        foreach(static::$navlinks as $k=>$v){
+        foreach(self::$navlinks as $k=>$v){
             if($k == $name){
                 $v['active'] = true;
             }else{
                 $v['active'] = false;
             }
         }
-        $this->set_links();
-    }
-
-    private static function getLinks(){
-        return is_null(self::$_links) ? array() : self::$_links;
-    }
-
-    public static function addLink($link){
-        static::getLinks()[] = $link;
+        self::set_links();
     }
 
     public function initalize(){
-        $this->set_links();
+        self::$_view = $this->view;
+        self::set_links();
     }
     public function set_links(){
-            $this->view->navlinks = static::$navlinks;
+            static::$_view->navlinks = static::$navlinks;
     }
 }
