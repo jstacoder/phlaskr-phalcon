cc687dacf681c018e3327b347b5bffb540f8400a
kyle roux <kyle@level2designs.com>
2015-07-27 17:23:33 -0700

    x

--- app/controllers/SignupController.php
+++ app/controllers/SignupController.php
@@ -3,13 +3,14 @@
 class SignupController extends ControllerBase{
     public function indexAction(){
         $this->view->form = new SignupForm();
+        $this->view->form->initalize();
         $this->view->form_file = 'signup/index.volt';
         echo $this->view->render('signup');
     }
     public function registerAction(){
         //echo '<link rel=stylesheet href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />';
         $user = new Users();    
-        if($this->request->getPost('password') == $this->request->getPost('confirm')){
+        if($this->request0>isPost() && $this->request->getPost('password') == $this->request->getPost('confirm')){
             $user->name = $this->request->getPost('name');
             $user->email = $this->request->getPost('email');
             $user->setPassword($this->request->getPost('password'));
