4c4ee932c643e3cedaa2d0188c61a85967772f5b
kyle roux <kyle@level2designs.com>
2015-07-27 17:27:10 -0700

    x

--- app/controllers/SignupController.php
+++ app/controllers/SignupController.php
@@ -3,32 +3,31 @@
 class SignupController extends ControllerBase{
     public function indexAction(){
         $this->view->form = new SignupForm();
-        $this->view->form->initalize();
         $this->view->form_file = 'signup/index.volt';
         echo $this->view->render('signup');
     }
     public function registerAction(){
         //echo '<link rel=stylesheet href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />';
-        $user = new Users();    
+        $user = new Users();
         if($this->request0>isPost() && $this->request->getPost('password') == $this->request->getPost('confirm')){
             $user->name = $this->request->getPost('name');
             $user->email = $this->request->getPost('email');
             $user->setPassword($this->request->getPost('password'));
             $success = $user->save();
-            if($success){                
-                echo $this->flash->success("Thanks for registering $user->email");                                
-            }else{  
-                echo $this->flash->error("error");                            
+            if($success){
+                echo $this->flash->success("Thanks for registering $user->email");
+            }else{
+                echo $this->flash->error("error");
                 foreach($user->getMessages() as $msg){
-                    echo $this->flash->error($msg);                    
+                    echo $this->flash->error($msg);
                 }
             }
             $this->dispatcher->forward(
               array(
                     "controller"=>"signup",
                     "action"=>"index"
-                )  
+                )
             );
-        }   
+        }
     }
 }
